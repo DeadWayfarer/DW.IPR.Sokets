677f97b4a12cf3c67905ed36d997e43db3077ec2
Danil Ober <ober2013@mail.ru>
2025-01-11 23:00:12 +0700

    add: пример сокетов для UDP

---
+++ DW.IPR.Sokets.UDPClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+// Даем команду на подключение
+Console.WriteLine("Нажмите любую кнопку чтобы подключиться к серверу...");
+Console.ReadKey();
+
+// Отправка данных серверу
+UdpClient udpClient = new UdpClient();
+IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8888);
+string message = "Привет, сервер!";
+Console.WriteLine("Нажмите любую кнопку чтобы отправить сообщение серверу...");
+Console.ReadKey();
+byte[] data = Encoding.UTF8.GetBytes(message);
+udpClient.Send(data, data.Length, serverEndPoint);
+
+// Получаем ответ от сервера
+byte[] receivedData = udpClient.Receive(ref serverEndPoint);
+string receivedMessage = Encoding.UTF8.GetString(receivedData);
+Console.WriteLine("Получено от сервера: " + receivedMessage);
---
+++ DW.IPR.Sokets.UDPServer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+UdpClient udpServer = new UdpClient(8888);
+IPEndPoint clientEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+while (true)
+{
+    byte[] receivedData = udpServer.Receive(ref clientEndPoint);
+    string receivedMessage = Encoding.UTF8.GetString(receivedData);
+    Console.WriteLine("Получено от клиента: " + receivedMessage);
+
+    string responseMessage = "Сообщение получено!";
+    byte[] responseData = Encoding.UTF8.GetBytes(responseMessage);
+    udpServer.Send(responseData, responseData.Length, clientEndPoint);
+}
