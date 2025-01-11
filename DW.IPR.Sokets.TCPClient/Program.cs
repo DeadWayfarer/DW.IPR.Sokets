3f183ff6c81a0f03bca8556e0fca834da2e16e43
Danil Ober <ober2013@mail.ru>
2025-01-11 22:36:17 +0700

    add: сделано подлючение по TCP

---
+++ DW.IPR.Sokets.TCPClient/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+// Указываем IP-адрес и порт сервера
+string serverIp = "127.0.0.1";
+int port = 8888;
+
+// Даем команду на подключение
+Console.WriteLine("Нажмите любую кнопку чтобы подключиться к серверу...");
+Console.ReadKey();
+
+// Создаем TCP-сокет и подключаемся к серверу
+TcpClient client = new TcpClient(serverIp, port);
+Console.WriteLine("Подключено к серверу.");
+
+// Получаем поток для чтения и записи данных
+NetworkStream stream = client.GetStream();
+
+// Отправка данных серверу
+string message = "Привет, сервер!";
+Console.WriteLine("Нажмите любую кнопку чтобы отправить сообщение серверу...");
+Console.ReadKey();
+
+byte[] data = Encoding.UTF8.GetBytes(message);
+stream.Write(data, 0, data.Length);
+Console.WriteLine("Сообщение отправлено серверу.");
+
+// Чтение ответа от сервера
+byte[] buffer = new byte[256];
+int bytesRead = stream.Read(buffer, 0, buffer.Length);
+string receivedMessage = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+Console.WriteLine("Получено от сервера: " + receivedMessage);
+
+// Закрываем соединение
+client.Close();
+
+Console.ReadKey();
--- DW.IPR.Sokets.TCPServer/Program.cs
+++ DW.IPR.Sokets.TCPServer/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+// Указываем IP-адрес и порт для сервера
+IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+int port = 8888;
+
+// Создаем TCP-сокет
+TcpListener listener = new TcpListener(ipAddress, port);
+
+// Запускаем сервер
+listener.Start();
+Console.WriteLine("Сервер запущен. Ожидание подключений...");
+
+// Принимаем клиента
+TcpClient client = listener.AcceptTcpClient();
+Console.WriteLine("Клиент подключен.");
+
+// Получаем поток для чтения и записи данных
+NetworkStream stream = client.GetStream();
+
+// Чтение данных от клиента
+byte[] buffer = new byte[256];
+int bytesRead = stream.Read(buffer, 0, buffer.Length);
+string receivedMessage = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+Console.WriteLine("Получено от клиента: " + receivedMessage);
+
+// Отправка данных клиенту
+string responseMessage = "Сообщение получено!";
+byte[] responseData = Encoding.UTF8.GetBytes(responseMessage);
+stream.Write(responseData, 0, responseData.Length);
+Console.WriteLine("Ответ отправлен клиенту.");
+
+// Закрываем соединение
+client.Close();
+listener.Stop();
+
+Console.ReadKey();
